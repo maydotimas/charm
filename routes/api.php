136483f5280b52f7097371e1fa2a694953d3569c
May DOtimas <dotimasmay@gmail.com>
2019-09-30 18:11:19 +0800

    Update

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,21 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('auth')->group(function () {
+
+    Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
+    Route::get('refresh', 'AuthController@refresh');
+
+    Route::group(['middleware' => 'auth:api'], function () {
+
+        Route::get('user', 'AuthController@user');
+        Route::post('logout', 'AuthController@logout');
+
+        // Users
+        Route::get('users', 'UserController@index')->middleware('isAdmin');
+        Route::get('users/{id}', 'UserController@show')->middleware('isAdminOrSelf');
+
+    });
+});
